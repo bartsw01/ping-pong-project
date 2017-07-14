89c19f4f5ab3193e955ad70a6990ac3ec35ec4d8
Bart Webber <bart@aseenrichment.com>
2017-07-14 15:04:00 -0700

    separate business logic and UI logic sections in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,60 @@
 //Business Logic
+var inputNumber = "";
+
+var pingpong = function(inputNumber){
+var inputNumberArray = [];
+var currentNumber = 0;
+var countTo = parseInt(inputNumber);
+var countBy = 1;
+var outputString = "";
+var outputArray = [];
+  for (var currentNumber = countBy; currentNumber <= countTo; currentNumber += countBy){
+    inputNumberArray.push(currentNumber);
+  }
+  for(var index = 0; index < inputNumberArray.length; index += 1){
+    arrayNumber = inputNumberArray[index];
+    if (arrayNumber%5 === 0 && arrayNumber%3 === 0){
+      arrayNumber = "ping-pong"
+    } else if (arrayNumber%5 === 0){
+      arrayNumber = "pong";
+    } else if (arrayNumber%3 === 0){
+      arrayNumber = "ping";
+    } else {
+      arrayNumber = arrayNumber;
+    }
+    outputArray.push(arrayNumber);
+  }
+alert(inputNumber);
+alert(inputNumberArray);
+alert(inputNumberArray.length);
+};
 
 
 
 // UI Logic
 $(document).ready(function() {
   $(".pingpong-form").submit(function(event) {
-    event.preventDefault();
+    inputNumber = $("#userNumber").val();
     $('ul').empty();
+    $("ul").append("<li>" + arrayNumber + "<li>");
+    event.preventDefault();
 
-    // outputArray.forEach(function(outPutListItem){
-    //   $("ul").append("<li>" + outPutListItem + "<li>");
-    // }
 
-    var inputNumber = $("#userNumber").val();
-    alert(inputNumber)
-    var inputNumberArray = [];
-    var currentNumber = 0;
-    var countTo = parseInt(inputNumber);
-    var countBy = 1;
-    var outputString = "";
-    var outputArray = [];
-    for (var currentNumber = countBy; currentNumber <= countTo; currentNumber += countBy){
-      inputNumberArray.push(currentNumber);
-    }
-    alert(inputNumberArray);
-    alert(inputNumberArray.length);
-    alert(inputNumberArray[2]);
-    for(var index = 0; index < inputNumberArray.length; index += 1){
-      arrayNumber = inputNumberArray[index];
-      if (arrayNumber%5 === 0 && arrayNumber%3 === 0){
-        arrayNumber = "ping-pong"
-      } else if (arrayNumber%5 === 0){
-        arrayNumber = "pong";
-      } else if (arrayNumber%3 === 0){
-        arrayNumber = "ping";
-      } else {
-        arrayNumber = arrayNumber;
-      }
-      $("ul").append("<li>" + arrayNumber + "<li>");
+
+
+//Business Logic
+
+
+      // $("ul").append("<li>" + arrayNumber + "<li>");
       // outputString = outputString + arrayNumber;
       // return outputArray;
 
 
-    }
-    // alert(outputString);
-    // alert(outputArray);
+    alert(outputArray);
 
 
 
-    // alert(outputArray);
+
 
 
 
